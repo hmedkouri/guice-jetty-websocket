7925f75dd42f76881158f4cca8ed89d12f919f23
Hicham Medkouri <hicham.medkouri@gmail.com>
2017-12-06 19:09:53 +0000

    modified the jetty wrapper config

--- chat-app/src/main/java/com/acuo/experimental/App.java
+++ chat-app/src/main/java/com/acuo/experimental/App.java
@@ -7,38 +7,58 @@ import com.acuo.common.app.main.Main;
 import com.acuo.experimental.tyrus.TyrusModule;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
+import com.google.inject.AbstractModule;
 import com.google.inject.Module;
+import com.google.inject.name.Names;
 import org.eclipse.jetty.util.resource.Resource;
 
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Provider;
 import java.util.Collection;
 
 public class App extends Main {
 
-    private JettyServerWrapperConfig config(String resourceDir) {
+    static class JettyServerWrapperConfigProvider implements Provider<JettyServerWrapperConfig> {
 
-        JettyResourceHandlerConfig chat = new JettyResourceHandlerConfig()
-                .withBaseResource(Resource.newClassPathResource(resourceDir))
-                .withWelcomeFiles(Lists.newArrayList("index.html"))
-                .withEtags(true)
-                .withContextPath("/");
+        private final String resourceDir;
 
-        return new JettyServerWrapperConfig()
-                .withContextPath("/")
-                .withApiPath("/api")
-                .withWebSocketSupport()
-                .withResourceHandlerConfig(chat)
-                .withHttpServerConnectorConfig(JettyServerConnectorConfig.forHttp("localhost", 8080));
+        @Inject
+        JettyServerWrapperConfigProvider(@Named("dir") String resourceDir) {
+            this.resourceDir = resourceDir;
+        }
+
+        public JettyServerWrapperConfig get() {
+
+            JettyResourceHandlerConfig chat = new JettyResourceHandlerConfig()
+                    .withBaseResource(Resource.newClassPathResource(resourceDir))
+                    .withWelcomeFiles(Lists.newArrayList("index.html"))
+                    .withEtags(true)
+                    .withContextPath("/");
+
+            return new JettyServerWrapperConfig()
+                    .withContextPath("/")
+                    .withApiPath("/api")
+                    .withWebSocketSupport()
+                    .withResourceHandlerConfig(chat)
+                    .withHttpServerConnectorConfig(JettyServerConnectorConfig.forHttp("localhost", 8080));
+        }
     }
 
 
     @Override
-    public JettyServerWrapperConfig config() {
-        return config("/tyrus");
+    public Class<? extends Provider<JettyServerWrapperConfig>> config() {
+        return JettyServerWrapperConfigProvider.class;
     }
 
     @Override
     public Collection<Module> modules() {
-        return ImmutableList.of(new TyrusModule());
+        return ImmutableList.of(new AbstractModule() {
+            @Override
+            protected void configure() {
+                bindConstant().annotatedWith(Names.named("dir")).to("/tyrus");
+            }
+        }, new TyrusModule());
     }
 
     public static void main(String[] args) {
